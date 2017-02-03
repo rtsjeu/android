3f1a0d817d230fe45bd1bb9bc4034ceced26c70a
sebastien <sebastien.chauvin@gmail.com>
2017-02-03 11:31:02 +0100

    Rules activity

--- app/src/main/java/ch/rts/mobile/le/jeu/activities/RulesActivity.java
+++ app/src/main/java/ch/rts/mobile/le/jeu/activities/RulesActivity.java
@@ -4,13 +4,49 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.MenuItem;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import ch.rts.mobile.le.jeu.R;
 
 /**
  * Created by npietri on 02.02.17.
  */
 
 public class RulesActivity extends AppCompatActivity {
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme_MainActivity);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rules);
+        ButterKnife.bind(this);
+
+        setSupportActionBar(toolbar);
+        toolbar.setTitle(R.string.app_name);
+
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+        supportActionBar.setDisplayUseLogoEnabled(false);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+        }
+        return false;
+    }
 
     public static void start(Activity activity) {
         start(activity, null);
